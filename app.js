8275d8b019f7f55fd3c01b228bc3e99d35c1871c
Moholand <moho.akbarzadeh@gmail.com>
2021-09-17 16:27:58 +0430

    set data to LS

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// Init Storage
+const storage = new Storage;
+
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init Weather Object
-const weather = new Weather('London', 'uk');
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 
 // Init UI Class
 const ui = new UI;
@@ -7,11 +13,26 @@ const ui = new UI;
 // get weather load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+// Change Location Event
+document.getElementById('change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value; 
+  const country = document.getElementById('country').value; 
+
+  weather.changeLocation(city, country);
+
+  storage.setLocationData(city, country);
+
+  // Get and display weather
+  getWeather();
+
+  // Close Modal
+  $('#locModal').modal('hide');
+});
+
 function getWeather() {
   weather.getWeather()
   .then(results => {
     ui.paint(results);
-    console.log(results)
   })
   .catch(err => console.log(err));
 }
--- weather.js
+++ weather.js
@@ -1,13 +1,13 @@
 class Weather {
-  constructor(city, state) {
+  constructor(city, country) {
     this.apiKey = 'e7fc0c5598d9612051e0f8e82b7486e5';
     this.city = city;
-    this.state = state;
+    this.country = country;
   }
 
   // Fetch Weather from api
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city},${this.state}&appid=${this.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city},${this.country}&appid=${this.apiKey}`);
 
     const responseData = await response.json();
 
@@ -15,9 +15,9 @@ class Weather {
   }
 
   // Change the Weather location
-  changeLocation(city, state) {
+  changeLocation(city, country) {
     this.city = city;
-    this.state = state;
+    this.country = country;
   }
 
 }
